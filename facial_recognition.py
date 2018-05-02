f10821fc90d1daea3a6593d17ba1a9748e38dc6d
Brent Ritzema <brentritzema@users.noreply.github.com>
2018-05-02 11:49:55 -0400

    Cleaned up facial recognition file a bit.

--- facial_recognition.py
+++ facial_recognition.py
@@ -1,3 +1,11 @@
+# facial_recognition.py is an adaption of the work from FaceNet by David Sandberg and a use-case of it by Arun Mandal
+# This program simply functions as a server, which subscribes to an MQTT topic to receive images. Once an image is
+# received it is run through the system which detects faces and then tries to recognize them. Once a match is found (or
+# no match is found) it publishes to a seperate topic which the RasperryPi is subscribed to so that the Pi can act on the
+# information.
+
+# Authors: Toussaint Cruise, Brent Ritzema
+
 import tensorflow as tf
 import numpy as np
 import facenet
@@ -12,19 +20,24 @@ import time
 BROKER = "iot.cs.calvin.edu"
 PORT = 1883
 QOS = 1
-#  TOPIC = "data/faces"
 
+IMAGE_TOPIC='highmount/camera'
+IMAGE_FILE_NAME='last.jpg'
+AUTHORIZED_FOLDER_NAME='authorized'
+
+#This function is called when a message is recieved
+#It takes the image from the PI, writes it to a file, and then send it through the facial recognition system
 def on_message(mosq, obj, msg):                             # Function to retrieve file when received
-  print("received")
-  with open('face.jpg', 'wb') as fd:
+  print("received image")
+  with open(IMAGE_FILE_NAME, 'wb') as fd: #overwrites any old file
       fd.write(msg.payload)
-  img = cv2.imread('face.jpg')
+  img = cv2.imread(IMAGE_FILE_NAME)
   match = findMatch(img)
-  print(match)
+  print(match) 
 
 client = mqtt.Client("P2")                                # Start MQTT Client, NB changed to P2
-client.connect("iot.cs.calvin.edu", 1883, 60)                 # Connect to server
-client.subscribe("highmount/camera",0)
+client.connect(BROKER, PORT, 60)                 # Connect to server
+client.subscribe(IMAGE_TOPIC, QOS)
 
 client.on_message = on_message                            # This is key - it calls the function
 
@@ -37,6 +50,7 @@ factor = 0.709
 margin = 44
 input_image_size = 160
 
+#setting up the tensorflow session (facenet useses it)
 sess = tf.Session()
 
 # read pnet, rnet, onet models from align directory and files are det1.npy, det2.npy, det3.npy
@@ -54,12 +68,15 @@ embedding_size = embeddings.get_shape()[1]
 #dictionary of faces that are authorized
 authorized = {}
 
+# This function is the face detection and embedding function. It takes an image, detects all the faces within it, 
+# finds embeddings for every face, and returns a list of the faces via their corresponding embeddings.
 def getFace(img):
     faces = []
     img_size = np.asarray(img.shape)[0:2]
     bounding_boxes, _ = detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
     if not len(bounding_boxes) == 0:
         for face in bounding_boxes:
+            #A lot of values and calculations here are just left as default for face detection
             if face[4] > 0.50:
                 det = np.squeeze(face[0:4])
                 bb = np.zeros(4, dtype=np.int32)
@@ -73,26 +90,22 @@ def getFace(img):
                 faces.append({'face':resized,'rect':[bb[0],bb[1],bb[2],bb[3]],'embedding':getEmbedding(prewhitened)})
     return faces
 
+# This function is what allows for recognition. By generating values for particular features of a face, it allows
+# comparisons of those features to other faces.
 def getEmbedding(resized):
     reshaped = resized.reshape(-1,input_image_size,input_image_size,3)
     feed_dict = {images_placeholder: reshaped, phase_train_placeholder: False}
     embedding = sess.run(embeddings, feed_dict=feed_dict)
     return embedding
 
-def compare2face(img1,img2):
-    face1 = getFace(img1)
-    face2 = getFace(img2)
-    if face1 and face2:
-        # calculate Euclidean distance
-        dist = np.sqrt(np.sum(np.square(np.subtract(face1[0]['embedding'], face2[0]['embedding']))))
-        return dist
-    return -1
-
+# This function generates a python dictionary of names and their embeddings found in the authorized
+# folder. This allows a comparison of the received image to a set of authorized users. In order to be
+# authorized you only need to put one image of yourself in the authorized folder with the title as your name.
 def generateDictionary():
-    for filename in os.scandir("authorized"):
+    for filename in os.scandir(AUTHORIZED_FOLDER_NAME):
         if (filename.name.endswith(".png") or filename.name.endswith(".jpg")) and filename.is_file():
             name = os.path.splitext(filename.name)[0]
-            face = getFace(cv2.imread("authorized/" + filename.name))
+            face = getFace(cv2.imread(AUTHORIZED_FOLDER_NAME + "/" + filename.name))
             if face:
                 authorized[name] = face[0]
 
